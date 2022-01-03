e8d5f10e606dba0817543a1afeb21b515fdb4d6f
Robert Ritson <81387018+r2j2ritson@users.noreply.github.com>
2022-01-03 10:02:01 -0700

    Update VectorRuggednessMeasure.R

--- R/VectorRuggednessMeasure.R
+++ R/VectorRuggednessMeasure.R
@@ -29,70 +29,75 @@
 terra_vrm <- function(x, n = 1, s = 3, filepath = "",outname = "vrm.tif") {
   if(length(s) > 2) stop( "Specified window exceeds 2 dimensions")   
   scale.factor <- round(s^2, 0)
+  f = matrix(1,s,s) #focal window
   
   print('Setting file names...')
-  wopt<-list(gdal=c("COMPRESS=NONE", "of=COG"), datatype='INT1U')
   sout <- paste0(filepath,"/sloperad.tif")
   aout <- paste0(filepath,"/aspectrad.tif")
   xyout <- paste0(filepath,"/xyrast.tif")
   zout <- paste0(filepath,"/zout.tif")
   xout <- paste0(filepath,"/xout.tif")
+  xfinal <- paste0(filepath,"/xoutfinal.tif")
   yout <- paste0(filepath,"/yout.tif")
+  yfinal <- paste0(filepath,"/youtfinal.tif")
   xsumout <- paste0(filepath,"/xsumout.tif")
   ysumout <- paste0(filepath,"/ysumout.tif")
   zsumout <- paste0(filepath,"/zsumout.tif")
-  vrmout <- paste0(filepath,"/vrmout.tif")
+  resout <- paste0(filepath,"/resout.tif")
   
   print('Calculating slope (radians)')
-  slp <- terra::terrain(x, v="slope", unit="radians", neighbors=8, filename = sout) 
+  slp <- terra::terrain(x, v="slope", unit="radians", neighbors=8, filename = sout,overwrite=T) 
   
   print('Calculating aspect (radians)')
-  asp <- terra::terrain(x, v="aspect", unit="radians", neighbors=8, filename = aout) 	
+  asp <- terra::terrain(x, v="aspect", unit="radians", neighbors=8, filename = aout,overwrite=T) 	
   
   print('Calculating xy raster...')
-  sin.slp <- terra::app(slp, fun = sin, cores = n, filename = xyout, wopt = wopt)
-  
+  xyraster <- terra::app(slp, fun = function(i){sin(i)}, cores = n, filename = xyout,overwrite=T)
+
   print('Calculating z raster...')
-  cos.slp <- terra::app(slp, fun = cos, cores = n, filename = zout, wopt = wopt)
+  zraster <- terra::app(slp, fun = function(i){cos(i)}, cores = n, filename = zout,overwrite=T)
   rm(slp)
-  file.remove(sout)
-  
+
   print('Calculating x raster...')
-  sin.asp <- terra::app(asp, fun = sin, cores = n, filename = xout, wopt = wopt) * sin.slp
+  xraster1 <- terra::app(asp, fun = function(i){sin(i)}, cores = n, filename = xout, overwrite=T) 
+  xraster2 <- xraster1 * xyraster
+  terra::writeRaster(xraster2,xfinal)
+  rm(xraster1)
   
   print('Calculating y raster...')
-  cos.asp <- terra::app(asp, fun = cos, cores = n, filename = yout, wopt = wopt) * sin.slp
-  rm(asp,sin.slp)
+  yraster1 <- terra::app(asp, fun = function(i){cos(i)}, cores = n, filename = yout, overwrite=T) 
+  yraster2 <- yraster1 * xyraster
+  terra::writeRaster(yraster2,yfinal)
+  rm(yraster1,xyraster,asp)
   do.call(file.remove,list(aout,xyout))
   
   print('Calculating x Sum...')
-  x.sum <- terra::focal(sin.asp, w = s, fun="sum", filename = xsumout, wopt = wopt) 
-  rm(sin.asp)
-  file.remove(yout)
+  x.sum <- terra::focal(xraster2, w = f, fun=sum, na.rm = T, filename = xsumout, overwrite=T) 
+  rm(xraster2)
+  do.call(file.remove,list(xout,xfinal))
   
   print('Calculating y Sum...')
-  y.sum <- terra::focal(cos.asp, w = s, fun="sum", filename = ysumout, wopt = wopt)
-  rm(cos.asp)
-  file.remove(xout)
+  y.sum <- terra::focal(yraster2, w = f, fun=sum, na.rm = T, filename = ysumout, overwrite=T)
+  rm(yraster2)
+  do.call(file.remove,list(yout,yfinal))
   
   print('Calculating z Sum...')
-  z.sum <- terra::focal(cos.slp, w = s, fun="sum", filename = zsumout, wopt = wopt)
-  rm(cos.slp)
+  z.sum <- terra::focal(zraster, w = f, fun=sum, na.rm = T, filename = zsumout, overwrite=T)
+  rm(zraster)
   file.remove(zout)
   
-  print('Calculating vrm...')
-  vrm.fun <- function(x, y, z) {sqrt((x^2) + (y^2) + (z^2))}
+  print('Calculate resultant raster...')
+  r.fun <- function(x, y, z) {sqrt((x^2) + (y^2) + (z^2))}
   sum_sds <- terra::sds(x.sum, y.sum, z.sum)
-  r <- terra::lapp(sum_sds, fun = vrm.fun, cores = n, filename = vrmout, wopt = wopt)
+  r <- terra::lapp(sum_sds, fun = r.fun, filename = resout,overwrite=T)
   rm(sum_sds,x.sum,y.sum,z.sum)
   do.call(file.remove,list(xsumout,ysumout,zsumout))
   
-  print('Calculating final vrm...')
-  vrm.fun.final <- function(r) 1 - (r / scale.factor) 
-  vrm.final <- terra::app(r, fun = vrm.fun.final, scale.factor, cores = n, filename = paste0(filepath,"/",outname), wopt = wopt)
-  
+  print('Calculate Ruggedness raster...')
+  vrm.fun <- function(r){1 - (r/scale.factor)}
+  vrm <- terra::app(r, fun = vrm.fun, cores = n, filename = paste0(filepath,"/",outname), overwrite=T)
   rm(r)
-  file.remove(vrmout)
+  file.remove(resout)
   terra::tmpFiles(remove = T)
-  return(vrm.final)
+  return(vrm)
 }
