33f9c42f1136f4be888eeb1650ec30c72f866cc0
Robert Ritson <81387018+r2j2ritson@users.noreply.github.com>
2022-10-06 12:29:29 -0600

    Update aspect_conversion.R

--- R/aspect_conversion.R
+++ R/aspect_conversion.R
@@ -2,19 +2,46 @@
 #' @description Transform an aspect direction as a measure from an anchor direction
 #'
 #' @param aspect The aspect number (in degrees) that you want to convert. 
-#' @param anchor A number (in degrees) in which you want the new aspect to reference
+#' @param anchor A number (in degrees) in which you want the new aspect to reference.
 #' 
 #' @author Robert Ritson <robert.ritson@idfg.idaho.gov>
 #'
 #' @examples
 #' ## Not Run:
+#' # Calculate Aspects with different anchors
 #' df <- data.frame(Aspect = seq(0,360,1)) # Aspect
-#' df$Degrees_from_South <- abs(df$Aspect - 180) # Degrees from South (0 is due South, 180 degrees)
-#' df$Degrees_from_SouthWest <- abs(180-abs(df$Aspect - 45)) # Degrees from Southwest (0 is due southwest, 225 degrees)
-#' df
+#' df$Degrees_from_South <- convert_aspect(df$Aspect, 180) # Degrees from South (0 as due South, 180 degrees)
+#' df$Degrees_from_SouthWest <- convert_aspect(df$Aspect, 225) # Degrees from Southwest (0 as due SouthWest, 225 degrees)
+#' head(df)
 #'
-#' dfs <- convert_aspect(aspect = 0, anchor = 180) #Due North is 180 degrees from due South
-#' dfsw <- convert_aspect(aspect = 0, anchor = 225) #Due North is 135 degrees from due SouthWest
+#' # Plot Degree Aspects
+#' library(ggplot2)
+#' data <- data.frame(dir = df$Aspect, mag = df$Aspect)
+#' normal <- ggplot(data, aes(x=dir, y=mag)) + 
+#'   geom_bar(stat='identity') + 
+#'   coord_polar() + 
+#'   ggtitle("Normal Aspect") +
+#'   scale_x_continuous(breaks = c(0,45,90,135,180,225,270,315))
+#' normal
+#'
+#' data <- data.frame(dir = df$Aspect, mag = df$Degrees_from_South)
+#' dfs <- ggplot(data, aes(x=dir, y=mag)) + 
+#'   geom_bar(stat='identity') + 
+#'   coord_polar() + 
+#'   ggtitle("Degrees from South") +
+#'   scale_x_continuous(breaks = c(0,45,90,135,180,225,270,315))
+#' dfs
+#'
+#' data <- data.frame(dir = df$Aspect, mag = df$Degrees_from_SouthWest)
+#' dfsw <- ggplot(data, aes(x=dir, y=mag)) + 
+#'   geom_bar(stat='identity') + 
+#'   coord_polar() + 
+#'   ggtitle("Degrees from South-West") +
+#'   scale_x_continuous(breaks = c(0,45,90,135,180,225,270,315))
+#' dfsw
+#'
+#' convert_aspect(aspect = 0, anchor = 180) #Due North is 180 degrees from due South
+#' convert_aspect(aspect = 0, anchor = 225) #Due North is 135 degrees from due SouthWest
 #'
 #' ## End Not Run
 #'
